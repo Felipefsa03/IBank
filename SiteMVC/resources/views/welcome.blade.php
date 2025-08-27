445686659f8854486c38ac653d4f9a72280b6f18
Felipefsa03 <limalipe355@gmail.com>
2025-08-27 15:34:04 -0300

    Initial commit: add Laravel project structure

---
+++ SiteMVC/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IBank</title>
+</head>
+<body>
+    <header>
+        <H1>IBank</H1>
+        <ul>
+            <li><a href="#">Entrar</a></li>
+            <li><a href="#">Registrar</a></li>
+            <li>Contatos</li>
+            <li>Historia</li>
+        </ul>
+
+
+    </header>
+</body>
+</html>
